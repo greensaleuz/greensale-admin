bf9db56d73a2f0b82fb788fd75ccdb5cdaa3b44e
Yodgorovna <lobargoziyeva9@gmail.com>
2023-08-25 12:13:18 +0500

    added

--- src/main.ts
+++ src/main.ts
@@ -6,9 +6,12 @@ import App from './App.vue'
 import './index.css'
 import router from './router'
 import i18n from './locales/i18n'
+import VueApexCharts from "vue3-apexcharts";
+
 
 const app = createApp(App)
 
+app.use(VueApexCharts);
 app.use(createPinia())
 app.use(router)
 app.use(i18n)
--- src/views/dashboard/DashboardView.vue
+++ src/views/dashboard/DashboardView.vue
@@ -34,7 +34,58 @@ export default defineComponent({
       apiSellerCount: 0,
       apiStorageCount: 0,
       summa: 0,
-      apiUserCount: 0
+      apiUserCount: 0,
+      series: [{
+        name: 'Seller',
+        type: 'area',        
+        data: [0, 39, 54, 61, 12, 54, 32, 52, 44, 32, 31]
+      }, {
+        name: 'B qiymat',
+        type: 'line',
+        data: [55, 69, 45, 61, 43, 54, 37, 52, 44, 61, 43]
+      }],
+      chartOptions: {
+        chart: {
+          height: 350,
+          type: 'line',
+        },
+        stroke: {
+          curve: 'smooth'
+        },
+        fill: {
+          type: 'solid',
+          opacity: [0.35, 1],
+        },
+        labels: ['Dec 01', 'Dec 02', 'Dec 03', 'Dec 04', 'Dec 05', 'Dec 06', 'Dec 07', 'Dec 08', 'Dec 09 ', 'Dec 10', 'Dec 11'],
+        markers: {
+          size: 0
+        },
+        yaxis: [
+          {
+            title: {
+              text: 'Qiymat 1',
+            },
+          },
+          {
+            opposite: true,
+            title: {
+              text: 'Qiymat 2',
+            },
+          },
+        ],
+        tooltip: {
+          shared: true,
+          intersect: false,
+          y: {
+            formatter: function (y: number) {
+              if (typeof y !== "undefined") {
+                return y.toFixed(0) + " points";
+              }
+              return y;
+            }
+          }
+        }
+      }
     }
   },
   async mounted() {
@@ -45,14 +96,13 @@ export default defineComponent({
 
 <template>
   <!--end:: Dashboard-->
+  <div id="chart">
+    <apexchart type="line" height="350" :options="chartOptions" :series="series"></apexchart>
+  </div>
+
   <ul>
-    <DashboardViewComponent
-      :apiData="apiBuyerCount"
-      :countUser="apiUserCount"
-      :countSeller="apiSellerCount"
-      :countAgreed="apiStorageCount"
-      :summaAnnouncemet="summa"
-    />
+    <DashboardViewComponent :apiData="apiBuyerCount" :countUser="apiUserCount" :countSeller="apiSellerCount"
+      :countAgreed="apiStorageCount" :summaAnnouncemet="summa" />
   </ul>
   <!--begin:: Dashboard-->
 </template>
