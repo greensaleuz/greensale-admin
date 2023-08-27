f9bc26f20d33047330b292326184ef494aab7d08
Bakhtiyorovna <kralecabaxtiyorovna@gmail.com>
2023-08-27 11:58:33 +0500

    dfgds

--- src/components/annoncements/sellerannouncements/SellerDeleteComponent.vue
+++ src/components/annoncements/sellerannouncements/SellerDeleteComponent.vue
@@ -6,11 +6,12 @@
 
         },
         props:{
-            idcategory:Number
+            
         },
         data() {
             return {
-                showDeleteModal: false
+                showDeleteModal: false,
+                Idd: 0
             };
         },
         methods: {
@@ -22,9 +23,8 @@
             },
             async confirmDelete() {
                 debugger;
-                const response = await axios.delete("/api/admin/categories/"+this.idcategory);
-                console.log("mkkjnjhj"+response.status)
-                console.log("Deleting the category...");
+                this.Idd = localStorage.getItem('sellerById')
+                const response = await axios.delete("/api/admin/seller/post/"+this.Idd);
                 this.$router.push('categories')
                 this.closeDeleteModal();
                 location.reload();
@@ -61,7 +61,7 @@
                 </button>
                 <div class="p-6 text-center">
                     <h3 class="my-5 text-lg font-normal text-gray-500 dark:text-gray-400 ">
-                       {{ $t('categorydleteaccess')}}</h3>
+                       {{ $t('postdleteaccess')}}</h3>
                     <button  @click="closeDeleteModal" type="button"
                              class="text-gray-500 mr-6 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-gray-200 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-600">
                              {{$t('censel')}}</button>
